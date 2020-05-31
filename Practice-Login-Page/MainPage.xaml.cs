f5e00b785025f6a62f0769521b5e2169dad6fdb1
Matt Levine <51725771+mattlevine68@users.noreply.github.com>
2020-05-30 21:59:31 -0400

    Add files via upload

---
+++ Practice-Login-Page/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Edibubble
+{
+    public partial class MainPage : ContentPage
+    {
+        public ICommand ClickCommand => new Command<string>((url) =>
+        {
+            Browser.OpenAsync(new System.Uri(url), BrowserLaunchMode.SystemPreferred);
+        });
+        public MainPage()
+        {
+
+            InitializeComponent();
+            BindingContext = this;
+        }
+    }
+}
